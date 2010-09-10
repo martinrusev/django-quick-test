4a258bfb3dc607fb7986ca434a8e7074754f8789
Martin Rusev <martinrusev@zoho.com>
2010-09-10 12:51:09 +0300

    Updated setup.py

---
+++ django_better_tests/management/commands/create_test_db.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from optparse import make_option
+import sys
+from django.db import connection
+from django.conf import settings
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'Creates the test db'
+
+
+    def handle(self, *args, **options):
+        connection.creation.create_test_db(verbosity = 1, autoclobber=False)
+        call_command('migrate', database='test', app=None, 
+                     target=None, skip=False, merge=False, backwards=False, fake=False, db_dry_run=False, show_list=False)
+        
+        connection.close()
+        
+    
--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup, find_packages
- 
-version = '0.1.0'
- 
+
 setup(
     name='django_better_tests',
     version='0.1.0',
     description="Django test runner that separates test database creation and test running",
-    long_description=open('README.rst').read(),
-    classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Django",
-        "Environment :: Web Environment",
-    ],
+    long_description="",
     keywords='django, tests, nose,',
     author='Martin Rusev',
     author_email='martinmcloud@gmail.com',
@@ -22,5 +14,12 @@ setup(
     zip_safe=False,
     install_requires=['setuptools','nose',],
     include_package_data=True,
-    setup_requires=['setuptools_git'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
 ) 
+
+
